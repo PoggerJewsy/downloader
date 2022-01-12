f2dd9d18a4f992bfee08f9581174249039ca6263
Rayr <rayr.plays@gmail.com>
2022-01-12 18:55:33 +0330

    Code format

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::io::Cursor;
 use std::env;
+use std::io::Cursor;
 use std::panic;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 async fn fetch_url(url: String, file_name: String) -> Result<()> {
     let response = reqwest::get(url).await?;
     let mut file = std::fs::File::create(file_name)?;
-    let mut content =  Cursor::new(response.bytes().await?);
+    let mut content = Cursor::new(response.bytes().await?);
     std::io::copy(&mut content, &mut file)?;
     Ok(())
 }
@@ -19,8 +19,10 @@ async fn main() {
     let link = &args[1];
     let file_name = &args[2];
     if !link.is_empty() && !file_name.is_empty() {
-        println!("Link: {}" ,link);
-        println!("File name: {}" ,file_name);
-        fetch_url(link.to_string(), file_name.to_string()).await.unwrap();
+        println!("Link: {}", link);
+        println!("File name: {}", file_name);
+        fetch_url(link.to_string(), file_name.to_string())
+            .await
+            .unwrap();
     }
-} 
+}
